a264015dc81f110d6fea24a4f878588618f6371d
kacpmu <14279897-kacpmu@users.noreply.gitlab.com>
2023-04-13 21:20:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flashcards
+
+Flashcards done right.
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flashcards/features/home/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flashcards',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
