19971d9e805d288a057d1a9baef15828f8337426
kacpmu <14279897-kacpmu@users.noreply.gitlab.com>
2023-04-14 00:25:21 +0200

    Added FAB to HomeScreen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -7,6 +7,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
