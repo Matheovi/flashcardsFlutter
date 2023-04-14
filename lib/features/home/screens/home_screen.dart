33fc6a40a01e70673831090147882fced702f479
kacpmu <14279897-kacpmu@users.noreply.gitlab.com>
2023-04-14 13:27:14 +0200

    Added DeckView to HomeScreen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flashcards/features/home/widgets/deck_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,7 +7,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DeckView(
+                children: List.generate(
+                  10, (index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Center(
+                        child: Text('Dummy ${index + 1}')
+                      ),
+                    ),
+                  )
+                ),
+              ),
+            ],
+          ), 
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
         backgroundColor: Colors.green,
