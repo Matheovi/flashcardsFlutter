44a9a7903f57ff54a8b817c1ac743f66699bf5c0
kacpmu <14279897-kacpmu@users.noreply.gitlab.com>
2023-04-16 16:13:23 +0200

    Extracted NewFlashcard widget

--- lib/features/new_flashcard/new_flashcard_screen.dart
+++ lib/features/new_flashcard/new_flashcard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flashcards/features/new_flashcard/widgets/new_flashcard.dart';
 import 'package:flutter/material.dart';
 
 class NewFlashcardScreen extends StatelessWidget {
@@ -14,68 +15,7 @@ class NewFlashcardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: "Front Page",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: "Back Page",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Decks:',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 24.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Dummy 1, Dummy 2, Dummy 3',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: TextButton(
-                      onPressed: () => {},
-                      child: const Text('Add to decks'),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+        child: NewFlashcard(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: ()=>{navigateBack(context)},
@@ -87,3 +27,4 @@ class NewFlashcardScreen extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/features/new_flashcard/widgets/new_flashcard.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class NewFlashcard extends StatelessWidget {
+  const NewFlashcard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "Front Page",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: "Back Page",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Decks:',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Dummy 1, Dummy 2, Dummy 3',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () => {},
+                  child: const Text('Add to decks'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
