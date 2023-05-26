8fd1ef3470df4750d943575b4ffa7344e115ccde
Matheovi <wiatrok35@gmail.com>
2023-05-26 14:59:57 +0200

    Add card rotation animation

--- lib/features/training/presentation/widget/training_flashcard.dart
+++ lib/features/training/presentation/widget/training_flashcard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class TrainingFlashcard extends StatefulWidget {
   final String frontText;
@@ -16,6 +17,7 @@ class TrainingFlashcard extends StatefulWidget {
 
 class _TrainingFlashcardState extends State<TrainingFlashcard> {
   bool backShown = false;
+  double angle = 0;
   late String textShown;
 
   @override
@@ -24,28 +26,65 @@ class _TrainingFlashcardState extends State<TrainingFlashcard> {
     textShown = widget.frontText;
   }
 
+  void _flip() {
+    setState(() {
+      angle = (angle + pi) % (2 * pi);
+    });
+  }
+
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          setState(() {
-            backShown = !backShown;
-          });
-        },
-        child: Dismissible(
-          onDismissed: (direction) {
-            if (direction == DismissDirection.startToEnd) {
-              debugPrint("Learned!!!!!");
-            }
-            if (direction == DismissDirection.endToStart) {
-              debugPrint("NotLearned :(:(:(:(");
-            }
-          },
-          key: UniqueKey(),
-          child: Card(
-              child: Center(
-                  child: Text(
-            backShown ? widget.backText : widget.frontText,
-          ))),
-        ));
+    return TweenAnimationBuilder(
+        tween: Tween<double>(begin: 0, end: angle),
+        duration: Duration(milliseconds: 500),
+        builder: (BuildContext context, double val, _) {
+          if (val >= (pi / 2)) {
+            backShown = true;
+          } else {
+            backShown = false;
+          }
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(val),
+            child: GestureDetector(
+                onTap: () {
+                  _flip();
+                },
+                child: Dismissible(
+                  onDismissed: (direction) {
+                    if (direction == DismissDirection.startToEnd) {
+                      debugPrint("Learned!!!!!");
+                    }
+                    if (direction == DismissDirection.endToStart) {
+                      debugPrint("NotLearned :(:(:(:(");
+                    }
+                  },
+                  key: UniqueKey(),
+                  child: backShown ? buildBackCard() : buildFrontCard(),
+                )),
+          );
+        });
+  }
+
+  Card buildFrontCard() {
+    return Card(
+        child: Center(
+            child: Text(
+      widget.frontText,
+    )));
+  }
+
+  Transform buildBackCard() {
+    return Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.identity()
+        ..rotateY(pi),
+      child: Card(
+          child: Center(
+              child: Text(
+        widget.backText,
+      ))),
+    );
   }
 }
